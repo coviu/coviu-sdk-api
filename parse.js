0b942a9645fc1c19d8c0b39749e247da828d899a
Oehlman <nathan.oehlman@gmail.com>
2018-02-28 12:05:19 +1000

    Cleanup indentations

--- parse.js
+++ parse.js
@@ -3,12 +3,12 @@
 import type { Fee, JSONFee } from './types'
 */
 exports.toFee = function (value /*: ?JSONFee */) /*: ?Fee */ {
-    if (!value) return;
-    try {
-        var result = JSON.parse(value);
-        if (!result.amount || !result.currency) return;
-        return result;
-    } catch (e) {
-        return;
-    }
+  if (!value) return;
+  try {
+    var result = JSON.parse(value);
+    if (!result.amount || !result.currency) return;
+    return result;
+  } catch (e) {
+    return;
+  }
 };
--- types.js
+++ types.js
@@ -10,7 +10,7 @@ export type ParticipantRole = 'HOST' | 'GUEST';
 // Defines the options for new participants
 export type NewParticipant = {
 	+display_name: string,
-    +fee?: JSONFee,
+	+fee?: JSONFee,
 	+role: ParticipantRole,
 	+picture?: string,
 	+state?: string
@@ -18,8 +18,8 @@ export type NewParticipant = {
 
 // Defines the options for updating participants
 export type UpdateParticipant = {
-    +display_name?: string,
-    +fee?: JSONFee,
+	+display_name?: string,
+	+fee?: JSONFee,
 	+role?: ParticipantRole,
 	+picture?: string,
 	+state?: string
@@ -36,27 +36,27 @@ export type SessionCreate = {
 
 // Defines the options for when updating an existing session
 export type SessionUpdate = {
-    session_name: ?string,
-    start_time: string,
-    end_time: string,
-    picture: ?string
+	session_name: ?string,
+	start_time: string,
+	end_time: string,
+	picture: ?string
 };
 
 // Defines the options for when querying the sessions
 export type SessionPageQuery = {|
-    page?: number,
-    page_size?: number,
-    start_time?: string,
-    end_time?: string,
-    included_canceled?: boolean
+	page?: number,
+	page_size?: number,
+	start_time?: string,
+	end_time?: string,
+	included_canceled?: boolean
 |};
 
 // The response from a paged query
 export type PagedResponse<T> = {|
-    content: Array<T>,
-    more: boolean,
-    page: number,
-    page_size: number
+	content: Array<T>,
+	more: boolean,
+	page: number,
+	page_size: number
 |};
 
 // Alias for the fee (this may be replaced at some point as it's currently stored as a JSON string)
@@ -64,49 +64,49 @@ export type JSONFee = string;
 
 // The structure for the fee that is stored within the JSONFee string
 export type Fee = {
-    amount: number,
-    description: ?string,
-    currency: string
+	amount: number,
+	description: ?string,
+	currency: string
 };
 
 // The information for the participant in a call
 export type CallParticipant = {|
 	client_id: string,
-    deleted_at?: string,
+	deleted_at?: string,
 	display_name: string,
 	entry_url: string,
-    fee?: JSONFee,
+	fee?: JSONFee,
 	participant_id: string,
-    picture: ?string,
+	picture: ?string,
 	role: ParticipantRole,
 	session_id: string,
-    state: ?string
+	state: ?string
 |};
 
 // The information for a created call session
 export type CallSession = {|
-    +session_id: string,
-    +session_name: ?string,
-    +start_time: string,
-    +end_time: string,
-    +picture: ?string,
-    +team_id: string,
-    +client_id: string,
-    +participants: Array<CallParticipant>
+	+session_id: string,
+	+session_name: ?string,
+	+start_time: string,
+	+end_time: string,
+	+picture: ?string,
+	+team_id: string,
+	+client_id: string,
+	+participants: Array<CallParticipant>
 |};
 
 export type CoviuClientSDK = {|
-    sessions: {
-        addParticipant: (sessionId: string, participant: NewParticipant) => Runnable<CallParticipant>,
-        cancelSession: (sessionId: string) => Runnable<any>,
+	sessions: {
+		addParticipant: (sessionId: string, participant: NewParticipant) => Runnable<CallParticipant>,
+		cancelSession: (sessionId: string) => Runnable<any>,
 		createSession: (SessionCreate) => Runnable<?CallSession>,
-        deleteParticipant: (participantId: string) => Runnable<any>,
+		deleteParticipant: (participantId: string) => Runnable<any>,
 		deleteSession: (sessionId: string) => Runnable<?any>,
-        getParticipant: (participantId: string) => Runnable<CallParticipant>,
-        getSession: (sessionId: string) => Runnable<CallSession>,
-        getSessionParticipants: (sessionId: string) => Runnable<Array<CallParticipant>>,
-        getSessions: (query: SessionPageQuery) => Runnable<PagedResponse<CallSession>>,
-        updateParticipant: (participantId: string, update: UpdateParticipant) => Runnable<CallParticipant>,
-        updateSession: (sessionId: string, update: SessionUpdate) => Runnable<CallSession>,
+		getParticipant: (participantId: string) => Runnable<CallParticipant>,
+		getSession: (sessionId: string) => Runnable<CallSession>,
+		getSessionParticipants: (sessionId: string) => Runnable<Array<CallParticipant>>,
+		getSessions: (query: SessionPageQuery) => Runnable<PagedResponse<CallSession>>,
+		updateParticipant: (participantId: string, update: UpdateParticipant) => Runnable<CallParticipant>,
+		updateSession: (sessionId: string, update: SessionUpdate) => Runnable<CallSession>,
 	}
 |};
